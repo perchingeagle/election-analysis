1d11a8255131e51b2106c2540297981fed7a8afa
perchingeagle <perchingeagle@gmail.com>
2021-09-04 19:31:44 -0400

    Module's examples completed

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,14 @@ file_to_save = os.path.join('Resources', 'election_analysis.txt')
 
 total_votes = 0
 
+candidate_option = []
+
+candidates_votes = {}
+
+winning_count = 0
+winning_candidate = ''
+winning_percentage = 0
+
 with open(file_to_load, 'r') as election_data:
 
     file_reader = csv.reader(election_data)
@@ -15,14 +23,45 @@ with open(file_to_load, 'r') as election_data:
 
     for row in file_reader:
         total_votes += 1
+        candidate = row[2]
+        
+        # store the candidate's name once for tracking
+        if candidate not in candidate_option:
+            candidate_option.append(candidate)
+            
+            # start tracking the candidate's vote count
+            candidates_votes[candidate] = 0
 
-print(f"Total number of votes is {total_votes}")
+        # increment the count for each voter
+        candidates_votes[candidate] += 1
 
-# 1. The data we need to retrieve
-# 2. A complete list candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote.
+print(f"Total number of votes is {total_votes:,}\n")
+#print(candidate_option)
+#print(candidates_votes)
+
+for candidates_name in candidates_votes:
+
+    votes = candidates_votes[candidates_name]
+    percentage = (votes/total_votes) * 100
+    print(f"{candidates_name}: {percentage:.1f}% ({votes:,})\n")
+
+    if (votes > winning_count) and (percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = percentage
+        winning_candidate = candidates_name
+
+winning_summary = (
+    f"\n-------------------------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------------------------------------------\n"
+    )
+
+#print(f"The winner of the election is {winning_candidate}")
+
+print(winning_summary)
+    
 
 
 
