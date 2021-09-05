1bb2675d708f6e6016cd35273b10ffd1c19349fa
perchingeagle <perchingeagle@gmail.com>
2021-09-05 11:33:52 -0400

    Election data's format fixed

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,7 @@ winning_count = 0
 winning_candidate = ''
 winning_percentage = 0
 
+#Read the data from the csv file
 with open(file_to_load, 'r') as election_data:
 
     file_reader = csv.reader(election_data)
@@ -39,6 +40,7 @@ print(f"Total number of votes is {total_votes:,}\n")
 #print(candidate_option)
 #print(candidates_votes)
 
+#Calculate the winning votes
 for candidates_name in candidates_votes:
 
     votes = candidates_votes[candidates_name]
@@ -50,17 +52,37 @@ for candidates_name in candidates_votes:
         winning_percentage = percentage
         winning_candidate = candidates_name
 
+#Prepare and format the output data
+line = "-----------------------------"
+
+election_results = (
+    f"\n{line}\n"
+    f"Election Results\n"
+    f"{line}\n"
+    f"Total Votes: {total_votes:,}\n"
+    f""
+    )
+print(election_results, end="")
+
 winning_summary = (
-    f"\n-------------------------------------------------\n"
+    f"{line}\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"----------------------------------------------------\n"
+    f"{line}\n"
     )
+
+#Save output data to text file
+with open(file_to_save, 'w') as txt_file:
+
+    txt_file.write(election_results)
+    txt_file.write(winning_summary)
+
+
 
 #print(f"The winner of the election is {winning_candidate}")
 
-print(winning_summary)
+#print(winning_summary)
     
 
 
